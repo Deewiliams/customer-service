5f04d41dccf168e38fca1b9447e4847acf6811d9
Desire Irankunda <desireirankundawilliams@gmail.com>
2026-08-06 10:44:21 +0200

    cleaned the code

--- src/app.js
+++ src/app.js
@@ -7,16 +7,16 @@ dotenv.config();
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import chatRoutes from "./routes/chat.routes.js";
+import homeRoutes from "./routes/index.routes.js";
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
-
+app.use("/", homeRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
-app.use("/", "<h1>Welcome to AI Support Chatbot</h1>");
 
 export default app;
---
+++ src/routes/index.routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const router = express.Router();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+router.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "../views/home.html");
+
+  res.sendFile(filePath);
+});
+
+export default router;
