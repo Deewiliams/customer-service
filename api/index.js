a161cdf29f6efdeb05970f3d86af363c5a96561b
Desire Irankunda <desireirankundawilliams@gmail.com>
2026-08-06 09:55:45 +0200

    fix vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 
-import app from "../app.js";
-import connectDB from "../config/db.js";
+import app from "../src/app.js";
+import connectDB from "../src/config/db.js";
 
 // Connect to MongoDB
 await connectDB();
