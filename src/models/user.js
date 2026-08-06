fd93cb6a96dad2f4ab246d39069c9e95cf3ef3f5
Desire Irankunda <desireirankundawilliams@gmail.com>
2026-08-06 12:19:59 +0200

    resolve the route issue

--- src/models/user.js
+++ src/models/user.js
@@ -29,7 +29,9 @@ const userSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
-  }
+  },
 );
 
-export default mongoose.model("User", userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
