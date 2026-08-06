f93f09b036ade5815a00d9751231e6c3d244300e
Desire Irankunda <desireirankundawilliams@gmail.com>
2026-08-06 12:01:11 +0200

    test

--- api/index.js
+++ api/index.js
@@ -1,10 +1,24 @@
+// import "dotenv/config";
+
+// import app from "../src/app.js";
+// import connectDB from "../src/config/db.js";
+
+// // Connect to MongoDB
+// await connectDB();
+
+// // Export the Express app for Vercel
+// export default app;
 import "dotenv/config";
 
 import app from "../src/app.js";
 import connectDB from "../src/config/db.js";
 
-// Connect to MongoDB
+app.get("/test", (req, res) => {
+  res.json({
+    message: "Vercel function is working",
+  });
+});
+
 await connectDB();
 
-// Export the Express app for Vercel
 export default app;
