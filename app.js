c2291befb510519eeab3809b815f1a82e7f432e4
Desire Irankunda <desireirankundawilliams@gmail.com>
2026-08-07 02:06:08 +0200

    resolved the cors issues

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import homeRoutes from "./src/routes/index.routes.js";
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://ai-support-chat-vert.vercel.app/"],
+  origin: ["http://localhost:5173", "https://ai-support-frontend-blush.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
